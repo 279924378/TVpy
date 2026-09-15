d6fbb86b5f60415e99f505b5ae70de36a4dceea7
jwarrenrzflynn <mcdvyzyuud528@outlook.com>
2026-09-15 13:46:17 +0800

    媚惑山庄v3.2直接URL格式

--- py/18㊙️媚惑山庄.py
+++ py/18㊙️媚惑山庄.py
@@ -112,7 +112,7 @@ class Spider(BaseSpider):
             "vod_duration": "",
             "vod_content": "",
             "vod_play_from": "高清",
-            "vod_play_url": m3u8_url + "$$$第1集",
+            "vod_play_url": m3u8_url,
         }]
         return result
 
